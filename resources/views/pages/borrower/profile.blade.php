9f2f079a2499ce50f8a1f340e5f4a44bdf4a616f
Festine-Capio <festine@capioteknologi.co.id>
2021-03-15 01:05:36 +0700

    kesemula

--- resources/views/pages/borrower/profile.blade.php
+++ resources/views/pages/borrower/profile.blade.php
@@ -78,45 +78,7 @@
         <script src="{{ asset('/script/profile.js') }}"></script>
         <script src="{{asset('/js/calculator.js')}}"></script>
         <script src="{{asset('/js/simple-slider.js')}}"></script>
-        <script>
-        const identity_image = document.querySelector('#file');
-            identity_image.addEventListener('change', (e) => {
-                const [identity_image] = e.target.files;
-                const { name: fileName } =identity_image;
-                const fileNameNew = `${fileName} `;
-                document.querySelector('.file-name').textContent = fileNameNew;
-            });
-
-             
-             
-            
-    </script>
-   
-    
-    <script>
-  var inputs = document.querySelectorAll( '.file' );
-
-Array.prototype.forEach.call( inputs, function( input ) {
-  var label = input.nextElementSibling,
-              labelVal = label.innerHTML;
-
-  input.addEventListener( 'change', function( e ) {
-    var fileName = '';
-
-    if ( this.files && this.files.length > 1 ) {
-      fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-    } else {
-      fileName = e.target.value.split( '\\' ).pop();
-    }
-
-    if ( fileName ) {
-      label.querySelector( 'span' ).innerHTML = fileName;
-    } else {
-      label.innerHTML = labelVal;
-    }
-  });
-});
-    </script>
+        
 
 @endsection
 @endsection
