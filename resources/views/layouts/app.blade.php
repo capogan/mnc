50f8de7a63897400008855457d4295fd0a5792d0
capogan <ogan@capioteknologi.co.id>
2021-03-19 17:36:43 +0700

    selectize

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -135,7 +135,8 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
-        $('#city,#emergency_city,#province,#emergency_province').select2();
+        $('#emergency_city,#emergency_province').select2();
+        $('#city,#province').select2();
     });
 </script>
 @yield('js')
