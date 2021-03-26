74784a876df574fd5b1c22995c1fe635cb96f745
ctridcat <ridcat@cashtree.id>
2021-03-26 12:15:51 +0700

    ridcat

--- app/Helpers/HelpCreditScoring.php
+++ app/Helpers/HelpCreditScoring.php
@@ -142,7 +142,7 @@ class HelpCreditScoring {
                             ->first();      
         if(!$credibility_check){
 
-            return ['status' => false , 'score' => 0 , 'detail' => $detail, 'credit_score' => $score_first_step , 'message' => [ 'credit_limit' => 0 , 'credibiliti_status' => 0, 'credibiliti_percentage' => 0]];
+            return ['status' => false , 'score' => $score , 'detail' => $detail, 'credit_score' => $score_first_step , 'message' => [ 'credit_limit' => 0 , 'credibiliti_status' => 0, 'credibiliti_percentage' => 0]];
         }
 
         return ['status' => true , 'scores' =>$score , 'detail' => $detail , 'credit_score' => $score_first_step, 'message' => [ 'credit_limit' => $credibility_check->maximal_loan , 'credibiliti_status' =>$limit_of_loan->title, 'credibiliti_percentage' => $limit_of_loan->max .' %']];
