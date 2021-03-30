b188a45ad63c739d737e1321dff1b3e8c9ac126b
capogan <ogan@capioteknologi.co.id>
2021-03-30 12:47:14 +0700

    status

--- app/Http/Controllers/LenderController.php
+++ app/Http/Controllers/LenderController.php
@@ -48,6 +48,8 @@ class LenderController extends Controller
 
     public function information_business_add(Request $request){
 
+        echo "test";
+        die();
         $arr_request = $request->all();
 
         //Asset Value
