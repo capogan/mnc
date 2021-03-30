a9a1058883e96b83ef9c3ae300f7002fd7fc6c4b
capogan <ogan@capioteknologi.co.id>
2021-03-31 00:41:47 +0700

    status

--- resources/views/pages/lender/index.blade.php
+++ resources/views/pages/lender/index.blade.php
@@ -181,7 +181,12 @@
                                                             </div>
                                                             <div class="col">
                                                                 <h6>Wajib Pajak<span>*</span></h6>
-                                                                <input type="text"  class="form-control" placeholder="Wajib Pajak" id="taxpayer" name="taxpayer" value="{{isset($lender_profile->taxpayer) ? $lender_profile->taxpayer : ''}}">
+{{--                                                                <input type="text"  class="form-control" placeholder="Wajib Pajak" id="taxpayer" name="taxpayer" value="{{isset($lender_profile->taxpayer) ? $lender_profile->taxpayer : ''}}">--}}
+                                                                <select class="form-control" id="taxpayer" name="taxpayer">
+                                                                    <option value="">Pilih Status</option>
+                                                                    <option value="pkp">PKP</option>
+                                                                    <option value="non_pkp">Non PKP</option>
+                                                                </select>
                                                             </div>
                                                         </div>
 
