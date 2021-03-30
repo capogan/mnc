425055435589ab073a34d562c37df0f976189f82
capogan <ogan@capioteknologi.co.id>
2021-03-30 15:35:43 +0700

    status

---
+++ app/Http/Controllers/Api/UsersEKYCController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\UserEKYC;
+use Illuminate\Http\Request;
+
+class UsersEKYCController extends Controller
+{
+    public function index(Request $request){
+
+        $callback = $request->callback;
+
+        $ekyc = UserEKYC::create([
+            'callback'=>$request->callback,
+            'created_at'=>date('Y-m-d'),
+            'updated_at'=>date('Y-m-d'),
+        ]);
+
+        if($ekyc){
+            $json = [
+                "status"=> true,
+                "message"=> 'Data berhasil ditambahkan.',
+            ];
+        }else{
+            if($ekyc){
+                $json = [
+                    "status"=> false,
+                    "message"=> 'Data Error',
+                ];
+            }
+        }
+
+        return response()->json($json);
+    }
+}
