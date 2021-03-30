8a3b8d80b8934dde8d8e672a9a002b78644e1101
capogan <ogan@capioteknologi.co.id>
2021-03-31 01:13:16 +0700

    status

--- resources/views/pages/lender/sign.blade.php
+++ resources/views/pages/lender/sign.blade.php
@@ -7,10 +7,10 @@
             @if($loan)
                 <div class="card bg-light card-aggrement">
                     <div class="scrollable">
-                       
+
                         <div class="card-body">
                             <div class="text-center">
-                            <h2>PERJANJIAN PENDANAAN</h2>
+                            <h2>PERJANJIAN PEMBERIAN PINJAMAN</h2>
                             <h5>No : {{$loan->invoice_number}}</h5>
                             </div>
                             <p>Duis faucibus sed leo a maximus. Donec hendrerit velit lacus, eu molestie metus rutrum nonecenas quis purus bibendum arcu euismod elementum id in quam.</p>
@@ -33,7 +33,7 @@
                                 <li>Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting.</li>
                             </ul>
                             <button type="submit" onclick="updated_status('{{$loan->id}}','19')" class="btn btn-primary btn-sm pull-left">KLIK untuk tanda tangan</button>
-                       
+
                     </div>
                 </div>
                 </div>
@@ -44,7 +44,7 @@
                     <br/>
                     <br/>
                 <div class="row">
-                   
+
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="section-title mb60" style="text-align:center;">
                         <div class="alert alert-danger" role="alert">
