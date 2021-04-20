2a27378f43f2e3c932b30ee300581db5ccacb823
capogan <ogan@capioteknologi.co.id>
2021-04-20 15:41:26 +0700

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,10 +57,7 @@
                     <ul>
                         <li class="active"><a href="/" class="animsition-link">Home</a>
 
-                        @if (Auth::check())
-                            <li><a href="/register/borrower" class="animsition-link">profile</a>
-                            </li>
-                        @else
+                        @if (!Auth::check())
                             <li><a href="/lender" class="animsition-link">Pendanaan</a>
                             </li>
                             <li><a href="/pinjam" class="animsition-link">Peminjam</a>
@@ -81,55 +78,13 @@
             </div>
             <div class="col-xl-2 col-lg-2 col-md-2 col-sm-12 col-12 d-none d-xl-block d-lg-block mt-3">
             @if (Auth::check())
-                <!--<div class="btn-action">
-                           <a href="/logout" class="btn btn-danger">KELUAR</a> </div>
-                       </div>-->
-                    <div class="row">
-                        <div class="col">
-                            <div class="row mt-2 justify-content-end">
-                                <p class="text-right">{{Auth::user()->name}}</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="btn-group">
-
-                                <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
-                                    <span class="caret"></span>
-                                </button>
-                                <div class="dropdown-menu">
-                                    @if(Auth::user()->group == 'borrower')
-                                        <a class="dropdown-item" href="/profile">
-                                        @else(Auth::user()->group == 'borrower')
-                                        <a class="dropdown-item" href="/profile/lender">
-                                        @endif
-                                        <div class="row mt-2 mb-2">
-                                            <div class="col mt-2">
-                                                Profile
-                                            </div>
-                                            <div class="col">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a class="dropdown-item" href="/logout">
-                                        <div class="row mt-2 mb-2">
-                                            <div class="col mt-2">
-                                                Keluar
-                                            </div>
-                                            <div class="col mt-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="red" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
-                                                    <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
-                                                    <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    </a>
-
-                                </div>
-                            </div>
+                    <div class="dropdown">
+                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-user" aria-hidden="true"></i> {{Auth::user()->name}}
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Profile</a>
+                            <a class="dropdown-item" href="#">Keluar</a>
                         </div>
                     </div>
                 @else
