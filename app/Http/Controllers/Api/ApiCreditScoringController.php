ece76f79ab69a871c6bcfc76cca32a4b91f84ad2
Richard-simbolon <ridcat@cashtree.id>
2021-05-28 13:07:42 +0700

    ridcat

--- app/Http/Controllers/Api/ApiCreditScoringController.php
+++ app/Http/Controllers/Api/ApiCreditScoringController.php
@@ -58,6 +58,7 @@ class ApiCreditScoringController extends ApiController
     }
 
     public function check_my_credit_score(Request $request){    
+        header('Access-Control-Allow-Origin: *');
         $personal_info = PersonalInfo::select('personal_info.date_of_birth','personal_info.number_of_dependents','personal_business.*')
                         ->leftJoin('personal_business' ,'personal_business.uid' , 'personal_info.uid')
                         ->leftJoin('users_file' , 'users_file.uid' ,'personal_info.uid' )
