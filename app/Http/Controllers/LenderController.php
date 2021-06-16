46974598b2988bb30676bb358edfec490a42dab1
Richard-simbolon <ridcat@cashtree.id>
2021-06-16 14:50:10 +0700

    ridcat simbolon 13 jun

--- app/Http/Controllers/LenderController.php
+++ app/Http/Controllers/LenderController.php
@@ -839,15 +839,6 @@ class LenderController extends Controller
 
         $pathDocument = public_path('upload/document/credit_aggreement/' . str_replace(' ', '', $data['title'] . '_' . uniqid()) . '.pdf');
         PDF::loadView('agreement.credit_agreement_lender', $data)->save($pathDocument);
-
-        $create_borrower_file = $this->created_borrower_document($data , $borrower,$request->id);
-        if(!$create_borrower_file){
-            return $json = [
-                "status"=> 'error',
-                "message"=> 'Pinjaman tidak dapat didanai , terjadi kesalahan proses dokumen peminjam.',
-            ];
-        }
-        
         $send_to = [
             // [
             //     'email' => $borrower->digisigndata->email,
@@ -900,7 +891,8 @@ class LenderController extends Controller
                 'document_id' => $doc_id,
                 'request_loan_id' => Utils::decrypt($request->id),
                 'created_at' => date('Y-m-d H:i:s'),
-                'status' => 'active'
+                'status' => 'active',
+                'type'=>'lender'
             ]
         );
         if(!$create_doc_aggreement){
@@ -932,16 +924,102 @@ class LenderController extends Controller
         ];
     }
 
-    public static function created_borrower_document($data , $lender ,$id){
-        $pathDocument = public_path('upload/document/credit_aggreement/borrower/' . str_replace(' ', '', $data['title'] . '_' . uniqid()) . '.pdf');
-        PDF::loadView('agreement.credit_agreement_borrower', $data)->save($pathDocument);
+    public function borrower_sign_document_fund_aggreement(Request $request){
+        $check_balance = new BNI;
+        if(!isset($request->id)){
+            return $json = [
+                "status"=> 'error',
+                "message"=> 'Data tidak ditemukan.',
+            ];
+        }
+        $loan = LoanRequest::with('personal_info')
+        ->with('business_info')
+        ->with('scoring')
+        ->where('status' , '18')->where('id' ,Utils::decrypt($request->id))->first();
+        if(!$loan){
+            return $json = [
+                "status"=> 'error',
+                "message"=> 'Tidak dapat didanai.',
+            ];
+        }
+        $borrower = User::where('id' , $loan->uid)
+                    ->with('digisigndata')
+                    ->first();
+
+        $lender = User::where('id' ,Auth::id())
+                    ->with('digisigndata')
+                    ->first();
+        if(!$lender->digisigndata || !$borrower->digisigndata){
+            return $json = [
+                "status"=> 'error',
+                "message"=> 'Pinjaman tidak dapat didanai.',
+            ];
+        }
+        $data = [
+            'title' => 'PERJANJIAN PENGGUNAAN LAYANAN P2P LENDING',
+            'date_request_loan' => date('d m Y'),
+            'borrower' => $borrower,
+            'lender' => $lender,
+            'loan' => $loan
+        ];
+
+        $pathDocument = public_path('upload/document/credit_aggreement/' . str_replace(' ', '', $data['title'] . '_' . uniqid()) . '.pdf');
+        PDF::loadView('agreement.credit_agreement_lender', $data)->save($pathDocument);
+
+        $send_to = [
+            // [
+            //     'email' => $borrower->digisigndata->email,
+            //     'name' => $borrower->digisigndata->full_name
+            // ],
+            [
+                'email' => $lender->digisigndata->email,
+                'name' => $lender->digisigndata->full_name
+            ]
+        ];
+        $req_sign = [
+            // [
+            //     'name' => $borrower->digisigndata->full_name,
+            //     'email' => $borrower->digisigndata->email,
+            //     'aksi_ttd' => 'ttd',
+            //     'kuser' => null,
+            //     'user' => 'ttd2',
+            //     'page' => '4',
+            //     'llx' => '193',
+            //     'lly' => '13',
+            //     'urx' => '89.3',
+            //     'ury' => '192.3',
+            //     'visible' => 1
+            // ],
+            [
+                'name' => $lender->digisigndata->full_name,
+                'email' => $lender->digisigndata->email,
+                'aksi_ttd' => 'ttd',
+                'kuser' => null,
+                'user' => 'ttd1',
+                'page' => '4',
+                'llx' => '430',
+                'lly' => '192.3',
+                'urx' => '330',
+                'ury' => '193.7',
+                'visible' => 1
+            ]
+        ];
         $doc_id = date('Ymd').'_'.uniqid().'_'.$lender->id;
+        $digisign = new DigiSign;
+        $response = $digisign->upload_document($pathDocument , $doc_id ,true, 'Lender_Aggreement' ,false , $send_to, $req_sign , $lender->id , 'credit_agreement');
+        if(!$response){
+            return [
+                "status"=> 'error',
+                "message"=> 'Error ketika menyimpan data, silahkan coba beberapa saat lagi.',
+            ];
+        }
         $create_doc_aggreement = RequestLoanDocument::create(
             [
                 'document_id' => $doc_id,
-                'request_loan_id' => Utils::decrypt($id),
+                'request_loan_id' => Utils::decrypt($request->id),
                 'created_at' => date('Y-m-d H:i:s'),
-                'status' => 'active'
+                'status' => 'active',
+                'type'=>'lender'
             ]
         );
         if(!$create_doc_aggreement){
@@ -950,6 +1028,27 @@ class LenderController extends Controller
                 "message"=> 'Upload document gagal.',
             ];
         }
+        if(!$loan){
+            return $json = [
+                "status"=> 'error',
+                "message"=> 'Data tidak ditemukan.',
+            ];
+        }
+        $loan->status = '19';
+        $loan->lender_uid = Auth::id();
+        if($loan->save()){
+            $this->loan_request_log($loan , Auth::id() , 19);
+            return $json = [
+                "status"=> 'success',
+                "message"=> 'Data berhasil di update.',
+                "url"=> 'portofolio/detail?p='.$request->id,
+            ];
+        }
+
+        return $json = [
+            "status"=> 'error',
+            "message"=> 'Error ketika menyimpan data, silahkan coba beberapa saat lagi.',
+        ];
     }
     
 
