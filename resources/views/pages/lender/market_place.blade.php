538863c35d035f1fdb9df6b35230ce512942780f
capogan <ogan@capioteknologi.co.id>
2021-06-24 15:45:15 +0700

    page loading

--- resources/views/pages/lender/market_place.blade.php
+++ resources/views/pages/lender/market_place.blade.php
@@ -1,61 +1,35 @@
 @extends('layouts.app_lender')
 @section('content')
-    <div class="container containers-with-margin">
-        <div class="row">
-            <div class="wrapper-content bg-white">
-                <div class="section-scroll" id="section-feature">
-                    
-                    <div class="pinside60">
-                        <div class="row">
-                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div class="section-title mb60" style="text-align:center;">
-                                <div class="alert alert-danger" role="alert">
-                                     Status akun kamu masih dalam proses.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
 
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class=" ">
+        <!-- content start -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="wrapper-content bg-white pinside60">
                         <div class="row">
-                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div class="section-title mb60">
-                                    <h2>Kalkulator Simulasi Imbal Hasil</h2>
+                            <div class="offset-xl-3 col-xl-6 offset-lg-3 col-lg-6 col-md-12 col-sm-12 col-12">
+                                <div class="error-img mb60">
+                                    <img src="{{asset('images/error-img.png')}}" class="" alt="">
                                 </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="feature-icon mb30">
-                                    <div class="icon mb20"><i class="icon-dollar-bill icon-default icon-2x"></i></div>
-                                    <h3>Cash Loan Facility</h3>
-                                    <p>Maecenas in ultricies sem. Nunc eget orci mi. Sed porttitor s, tellus fringilla condimentum eglis elit dictum cerat. </p>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="feature-icon mb30">
-                                    <div class="icon mb20"><i class="icon-documentation icon-default icon-2x"></i></div>
-                                    <h3>Special Preapproved Loan</h3>
-                                    <p>Pellentesque mollis, diam a viverra luctus, nisl dui vehicula erat, id congue ante hicula tellus sit amet.</p>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="feature-icon mb30">
-                                    <div class="icon mb20"><i class="icon-money icon-default icon-2x"></i></div>
-                                    <h3>No Varifactions</h3>
-                                    <p>Our loan rates and charges are very attractive lorem ipsums sitamet uerse ipsum.Curabitulectus mattis vitae. </p>
-                                </div>
-                            </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="feature-icon mb30">
-                                    <div class="icon mb20"><i class="icon-dollar-coins icon-default icon-2x"></i></div>
-                                    <h3>150% Funding on Loan</h3>
-                                    <p>Nunc eget orci mi. Sed porttitor lacus quis scelerisque dignissim. Nullam bibendu msfeus, isapien dolor et dui.</p>
+                                <div class="error-ctn text-center">
+                                    <h2 class="msg">Maaf</h2>
+                                    <h1 class="error-title mb40">Proses Verifikasi</h1>
+                                    <p class="mb40">Status akun kamu masih dalam proses. Kami akan menghubungi Anda jika proses verifikasi telah berhasil</p>
+                                    <a href="index.html" class="btn btn-default text-center">kembali ke Beranda</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                
             </div>
         </div>
     </div>
