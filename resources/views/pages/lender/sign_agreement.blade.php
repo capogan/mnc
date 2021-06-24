07ac77b422e9dd5b0cc9fcd1019d84c59936c16c
capogan <ogan@capioteknologi.co.id>
2021-06-24 21:38:04 +0700

    lender

--- resources/views/pages/lender/sign_agreement.blade.php
+++ resources/views/pages/lender/sign_agreement.blade.php
@@ -70,9 +70,15 @@
 
                                                                         <p class="lead">PT. sistem informasi dan aplikasi pembiayaan [SIAP] menerapkan tanda tangan digital dalam setiap penandatanganan perjanjian pinjam meminjam uang dalam layanan SIAP.
                                                                             PT. SIAP menyediakan layanan tanda tangan elektronik yang baik dan berkualitas dalam membantu interaksi dengan peminjam dan pemberi pinjamannya.
-                                                                            Berikut ini adalah Informasi tentang Tanda Tangan Digital ;
+                                                                            Berikut ini adalah Informasi tentang Tanda Tangan Digital :
                                                                         </p>
-
+                                                                        <ul>
+                                                                            <li> PT, Siap dalam menyediakan layanan tanda tangan Eletronik bekerja sama dengan Platform Digisign. sehingga dalam hal ini Anda diharuskan dan akan di hubungkan untuk melakukan pendaftaran pada platform Digisign.</li>
+                                                                            <li>Tanda tangan digital adalah skema matematis yang digunakan untuk membuktikan keaslian pesan atau dokumen digital. Skema ini menjadi jaminan bahwa data dan informasi benar-benar berasal dari sumber yang benar Tanda tangan digital terdiri dari deret fungsi hash yang dihasilkan dari proses algoritme fungsi hash tertentu yang kemudian disandikan (dienkripsi) dengan algoritme kriftografi kunci asimetris. Untuk memverifikasinya digunakan kunci publik dari algoritme tesebut.</li>
+                                                                            <li>Tanda Tangan Digital telah tertera dalam UU Nomor 11 Tahun 2008 Pasal 11 ayat 1 tentang Informasi dan Transaksi Elektronik (UU ITE), lalu Peraturan Pemerintah Nomor 82 Tahun 2012 Pasal 52 ayat 1 dan 2 tentang Penyelenggaraan Sistem dan Transaksi Elektronik.</li>
+                                                                            <li>Baca dan pelajari secara baik semua isi dari perjanjian yang akan di tanda tangani.</li>
+                                                                            <li>Persiapkan Ponsel anda. karena anda akan di haruskan memasukan kode OTP sebagai tanda verifikasi. (dilarang memberikan informasi tentang kode OTP kepada siapapun. termaksud seseorang yang mengatasnamakan PT. SIAP</li>
+                                                                        </ul>
                                                                     <hr>
                                                                     @if($sign_agreement->status_activation != 'active')
                                                                         <a href="#" id="activate_account_dgsign" type="button" class="btn btn-primary">Aktivasi Akun</a>
