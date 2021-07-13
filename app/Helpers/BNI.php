70e1f264331df9b97c351907dd0dd7c034c0d134
Richard-simbolon <richard.simbolon28@gmail.com>
2021-07-13 13:44:10 +0700

    ridcat

--- app/Helpers/BNI.php
+++ app/Helpers/BNI.php
@@ -441,7 +441,7 @@ class BNI
             return false;
         }
         $data = [
-            "accountNumber" =>'8450000000000006'
+            "accountNumber" =>'9880045044352004'
         ];
         $body = $this->buildBodyPayload($data);
         if (time() >= strtotime($this->EXPIRES_AT)) {
