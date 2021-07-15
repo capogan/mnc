04f6fcd1269518fbf9c4f9e09909039f2aa3f5f2
Richard-simbolon <richard.simbolon28@gmail.com>
2021-07-15 09:15:13 +0700

    ridcat

--- app/Http/Controllers/Api/BNITest.php
+++ app/Http/Controllers/Api/BNITest.php
@@ -107,7 +107,7 @@ class BNITest  extends Controller
 
     public function payment_clearing(){
         $data = [
-            "accountNumber" => "1000031361",
+            "accountNumber" => "0316999605",
             "beneficiaryAccountNumber" => "123456",
             "beneficiaryAddress1"=>"Jakarta",
             "beneficiaryAddress2"=>"",
