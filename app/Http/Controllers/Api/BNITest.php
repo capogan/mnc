8444ecb47a92e29657ac8cd80ece29985121f72b
Richard-simbolon <richard.simbolon28@gmail.com>
2021-07-16 20:25:27 +0700

    ridcat

--- app/Http/Controllers/Api/BNITest.php
+++ app/Http/Controllers/Api/BNITest.php
@@ -146,7 +146,7 @@ class BNITest  extends Controller
 
     public function payment_interbank(){
         $data = [
-            "accountNumber" => "1000079187",
+            "accountNumber" => "0317246677",
             "beneficiaryAccountNumber"=>"0011223344",
             "beneficiaryAccountName"=>"SARI ANGGRAINI",
             "beneficiaryBankCode"=>"542",
